d86ae752d4340a45f987a9622854d06932188374
Matias Vallejos <matiavallejo@gmail.com>
2022-12-02 03:18:43 -0300

    Added part two and eslint

--- Day1/script.js
+++ Day1/script.js
@@ -1,35 +1,42 @@
 const fs = require('fs');
 
+const findTop = (elves) => {
+  const sums = [];
+
+  for (let i = 0; i < elves.length; i++) {
+    let totalCalories = 0;
+    for (let j = 0; j < elves[i].length; j++) {
+      totalCalories += elves[i][j];
+    }
+
+    sums.push(totalCalories);
+  }
+
+  sums.sort((a, b) => b - a);
+
+  return [sums[0], sums[1], sums[2]];
+};
+
 fs.readFile('./input.txt', 'utf-8', (err, data) => {
-    if(err){
-        console.error(err);
-        return;
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const split = data.split('\n');
+  const parsedData = [];
+  let elf = [];
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] === '') {
+      parsedData.push(elf);
+      elf = [];
+    } else {
+      elf.push(parseInt(split[i], 10));
     }
+  }
+  const top = findTop(parsedData);
+  const totalTopCalories = top.reduce((a, v) => a + v);
 
-
-    let split = data.split('\n');
-    let parsedData = [];
-    let elve = []
-    for(let i = 0; i < split.length; i++){
-        if(split[i] == ''){
-            parsedData.push(elve);
-            elve = []
-        }else{
-            elve.push(parseInt(split[i]));
-        }
-    }
-
-    let maxCalories = 0;
-    for(let i = 0; i < parsedData.length; i++){
-        let totalCalories = 0;
-        for(let j = 0; j < parsedData[i].length; j++){
-            totalCalories += parsedData[i][j];
-        }
-
-        if(totalCalories > maxCalories){
-            maxCalories = totalCalories;
-        }
-    }
-
-    console.log(maxCalories);
-})
+  console.log(`The elf carrying most calories is carrying ${top[0]}`);
+  console.log(`The top three elves are carrying in total ${totalTopCalories}`);
+});
